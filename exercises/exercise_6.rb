09a38165a70585d03cfeeb1e9dd99233d8df7ee8
DASitby <davidsooley551@gmail.com>
2023-05-06 21:33:28 +0000

    complete sixth exercise

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "David", last_name: "Sooley", hourly_rate: 60)
+@store1.employees.create(first_name: "Jacob", last_name: "Newman", hourly_rate: 60)
+@store2.employees.create(first_name: "Khorra", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Kiley", last_name: "Billard", hourly_rate: 60)
+@store2.employees.create(first_name: "Jhomas", last_name: "Loader", hourly_rate: 60)
